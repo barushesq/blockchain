9261691da401b14caec4a7298d71cb1dbd65c938
barushesq <barushesq@gmail.com>
2024-04-03 13:34:11 -0600

    Web 3.0 blockchain sin login

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
-import "./index.css";
 import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-function App() {
-  const respuestaGoogle=(respuesta)=>{
-    console.log(respuesta);
-    console.log(respuesta.profileObj);
-  }
-
-  return (
+const App = () => (
     <div className="min-h-screen">
-      <GoogleLogin
-        clientId='159932420890-pdthvht0u1v7sqius893bmb8itijobto.apps.googleusercontent.com'
-        buttonText='Login'
-        onSuccess={respuestaGoogle}
-        onFailure={respuestaGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Welcome />
-      </div>
-      <Services />
-      <Transactions />
-      <Footer />
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  );
-}
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
 export default App;
