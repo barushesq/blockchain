c25f49f8b440bda56249f5b658eefa444fc1f629
barushesq <barushesq@gmail.com>
2024-04-04 09:55:54 -0600

    Mod estilos 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,32 +18,25 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4 relative">
-      <div className="flex items-center md:flex-[0.1] flex-initial">
+      <div className="md:flex-[0.1] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
-      <ul className="text-white md:flex list-none flex-row justify-between items-center flex-initial">
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {[].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
       </ul>
-      <div className="absolute top-0 right-0">
-        {!toggleMenu ? (
-          <HiMenuAlt4
-            fontSize={28}
-            className="text-black md:hidden cursor-pointer mr-4" // Ajusta el margen aquí
-            onClick={() => setToggleMenu(!toggleMenu)}
-          />
-        ) : (
-          <AiOutlineClose
-            fontSize={28}
-            className="text-black md:hidden cursor-pointer mr-4" // Ajusta el margen aquí
-            onClick={() => setToggleMenu(!toggleMenu)}
-          />
+      <div className="absolute top-0 right-0 mr-10">
+        {!toggleMenu && (
+          <HiMenuAlt4 fontSize={100} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
+        )}
+        {toggleMenu && (
+          <AiOutlineClose fontSize={100} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
         )}
       </div>
       {toggleMenu && (
         <ul
-          className="z-10 fixed top-13 right-0 p-3 w-[70vw] h-screen shadow-2xl list-none
+          className="z-10 fixed top-0 right-0 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
           flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
         >
           <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
